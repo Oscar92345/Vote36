d9275f4f3996081c95f5d054bff8162cb3872f20
Oscar92345 <s11231124@nhsh.tp.edu.tw>
2025-04-14 22:26:28 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBB_8IKxjnzr9zZPnMLjGH75fqT7BgE4uA",
+  authDomain: "winddabatevote.firebaseapp.com",
+  databaseURL: "https://winddabatevote-default-rtdb.firebaseio.com",
+  projectId: "winddabatevote",
+  storageBucket: "winddabatevote.appspot.com",
+  messagingSenderId: "519338469519",
+  appId: "1:519338469519:web:7b1eea8ee94101f094e8fa"
+};
+firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -1,35 +1,14 @@
-const PHASE = 'impression'; // 可以改成 'result' 表示第二輪投票
 
-const STORAGE_KEY = `vote_${PHASE}`;
-const isAdmin = window.location.search.includes('admin=1');
-
-const vote = (side) => {
-  if (localStorage.getItem(STORAGE_KEY)) {
-    document.getElementById('status').innerText = '你已經投過票囉！';
+function vote(category, side) {
+  const votedKey = category + '_voted';
+  if (localStorage.getItem(votedKey)) {
+    document.getElementById('status').textContent = '你已經投過票了！';
     return;
   }
-  fetch(`https://winddebatevote-default-rtdb.firebaseio.com/${PHASE}/${side}.json`, {
-    method: 'POST',
-    body: JSON.stringify(true)
-  }).then(() => {
-    localStorage.setItem(STORAGE_KEY, side);
-    document.getElementById('status').innerText = '投票成功！謝謝你！';
-  });
-};
 
-const showResults = () => {
-  Promise.all(['pro', 'con'].map(side =>
-    fetch(`https://winddebatevote-default-rtdb.firebaseio.com/${PHASE}/${side}.json`).then(res => res.json())
-  )).then(([pro, con]) => {
-    const proVotes = pro ? Object.keys(pro).length : 0;
-    const conVotes = con ? Object.keys(con).length : 0;
-    document.getElementById('results').innerHTML = `
-      <p>正方：${proVotes} 票</p>
-      <p>反方：${conVotes} 票</p>
-    `;
+  const ref = firebase.database().ref('votes/' + category + '/' + side);
+  ref.transaction(current => (current || 0) + 1).then(() => {
+    localStorage.setItem(votedKey, 'true');
+    document.getElementById('status').textContent = '投票成功，謝謝你！';
   });
-};
-
-if (isAdmin) {
-  document.getElementById('admin-section').style.display = 'block';
 }
